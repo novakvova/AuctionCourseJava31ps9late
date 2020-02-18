113eb899fbc9a9f3d5c77c609a624e75030bc8ff
novakvova <novakvova@gmail.com>
2020-02-18 11:31:09 +0200

    Add conection in database

--- carShowRoom/src/car/show/room/service/CustomAuthenticationProvider.java
+++ carShowRoom/src/car/show/room/service/CustomAuthenticationProvider.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,11 +19,16 @@ import car.show.room.pojo.User;
 
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
+	//@Autowired
+	private Session session; // This will auto-inject the authentication service into the controller.
 	private static List<User> users = new ArrayList<User>();
 
-	public CustomAuthenticationProvider() {
+	public CustomAuthenticationProvider(SessionFactory sessionFactory) {
 		users.add(new User("erin", "123", "ROLE_ADMIN"));
 		users.add(new User("mike", "234", "ROLE_ADMIN"));
+		session = sessionFactory.openSession();
+		System.out.println("------Connection good---Security--");
+		//session.close();
 	}
 
 	@Override
@@ -28,12 +36,12 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 		String name = authentication.getName();
 		Object credentials = authentication.getCredentials();
 		System.out.println("credentials class: " + credentials.getClass());
-		
+
 		if (!(credentials instanceof String)) {
 			return null;
 		}
 		String password = credentials.toString();
-		
+
 		Optional<User> userOptional = users.stream().filter(u -> u.match(name, password)).findFirst();
 
 		if (!userOptional.isPresent()) {
